3fd766f4a4f9863e7a67907b69a2aa0e357ad392
sameshdesai7 <srd00013@mix.wvu.edu>
2022-12-14 08:00:37 -0500

    Updated Documentation

--- src/app/common/guard/auth.guard.ts
+++ src/app/common/guard/auth.guard.ts
@@ -1,14 +1,18 @@
+//Required Imports
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlSerializer } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
-import { User } from '../services/user';
+
 @Injectable({
   providedIn: 'root'
   
 })
+
+
 export class AuthGuard implements CanActivate {
   
+  //AuthService and Router initiated in constructor.
   constructor(
     public authService: AuthService,
     public router: Router
@@ -16,9 +20,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    //Calls Authervice to determine if user is logged in. If false redirect to login. 
     if(this.authService.isLoggedIn !== true) {
       this.router.navigate(['sign-in']) 
     }
+    //Else return true.
     return true;
   }
 }
